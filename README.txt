510721c06cbbc4df34b6eef3f515247e88c4190c
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-16 23:50:40 -0300

    Se logro funcionar el update excel de products(no quimicos) pero solo logra 1301 y son 1307 productos,a ver eso

---
+++ README.txt
@@ -0,0 +1,2 @@
+LA FORMULA DE EXCEL (update-excel) parece funcionar sin errores,pero solo encuentra 1301 elementos,
+y al perecer son 1307.
--- update-excel.php
+++ update-excel.php
@@ -1,42 +1,42 @@
 <?php
-
 require 'vendor/autoload.php'; // Asegúrate de incluir la biblioteca para leer archivos Excel, como PhpSpreadsheet
-
-
-
-
 require 'database.php'; // Incluye el archivo con la configuración de la conexión a la base de datos
 
 // Función para insertar o actualizar datos en la base de datos
-function insertOrUpdateDatos($productId, $code, $description, $presentation, $dealerPrice, $retailPrice, $conn) {
+function insertOrUpdateDatos($productId, $code, $description, $presentation, $dealerPrice, $retailPrice, $pdo) {
     try {
         // Verifica y ajusta los valores
-        if ($code === "#N/A") {
+        if ($code === "#N/A" || $code === "#REF!" || $code === "#VALUE!") {
             $code = "-";
         }
 
-        if ($dealerPrice == 42 && $retailPrice == 42) {
-            $dealerPrice = 0;
-            $retailPrice = 0;
-        }
+        // Verifica si dealerPrice es numérico, de lo contrario, establece 0
+$dealerPrice = is_numeric($dealerPrice) ? number_format($dealerPrice, 2, '.', '') : 0;
+
+// Verifica si retailPrice es numérico, de lo contrario, establece 0
+$retailPrice = is_numeric($retailPrice) ? number_format($retailPrice, 2, '.', '') : 0;
 
         // Consulta si 'code' ya existe en la base de datos
-        $stmt = $conn->prepare('SELECT * FROM productNormal WHERE code = ?');
+        $stmt = $pdo->prepare('SELECT * FROM productNormal WHERE code = ?');
         $stmt->execute([$code]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($row) {
             // Si 'code' existe, actualiza la fila existente
-            $stmt = $conn->prepare('UPDATE productNormal SET dealerPrice = ?, retailPrice = ?, description = ?, presentation = ? WHERE code = ?');
+            $stmt = $pdo->prepare('UPDATE productNormal SET dealerPrice = ?, retailPrice = ?, description = ?, presentation = ? WHERE code = ?');
             $stmt->execute([$dealerPrice, $retailPrice, $description, $presentation, $code]);
 
             echo "Registro actualizado para el código $code\n";
+            echo "Dealer Price: " . number_format($dealerPrice, 2) . "\n";
+            echo "Retail Price: " . number_format($retailPrice, 2) . "\n";
         } else {
             // Si 'code' no existe, inserta una nueva fila
-            $stmt = $conn->prepare('INSERT INTO productNormal (productId, code, description, presentation, dealerPrice, retailPrice) VALUES (?, ?, ?, ?, ?, ?)');
+            $stmt = $pdo->prepare('INSERT INTO productNormal (productId, code, description, presentation, dealerPrice, retailPrice) VALUES (?, ?, ?, ?, ?, ?)');
             $stmt->execute([$productId, $code, $description, $presentation, $dealerPrice, $retailPrice]);
 
             echo "Nuevo registro insertado para el código $code\n";
+            echo "Dealer Price: " . number_format($dealerPrice, 2) . "\n";
+            echo "Retail Price: " . number_format($retailPrice, 2) . "\n";
         }
     } catch (PDOException $e) {
         echo "Error al insertar o actualizar datos en la base de datos: " . $e->getMessage() . "\n";
@@ -56,16 +56,15 @@ try {
         $code = $worksheet->getCell('A' . $rowNum)->getValue();
         $description = $worksheet->getCell('B' . $rowNum)->getValue();
         $presentation = $worksheet->getCell('C' . $rowNum)->getValue();
-        $dealerPrice = $worksheet->getCell('D' . $rowNum)->getValue();
-        $retailPrice = $worksheet->getCell('E' . $rowNum)->getValue();
+        $dealerPrice = $worksheet->getCell('D' . $rowNum)->getCalculatedValue();
+        $retailPrice = $worksheet->getCell('E' . $rowNum)->getCalculatedValue();
 
         if (!is_string($code)) {
             $code = strval($code);
         }
 
         if (!empty($code)) {
-            insertOrUpdateDatos($productId, $code, $description, $presentation, $dealerPrice, $retailPrice, $conn);
-            echo "$productId, $code, $description, $presentation, $dealerPrice, $retailPrice\n";
+            insertOrUpdateDatos($productId, $code, $description, $presentation, $dealerPrice, $retailPrice, $pdo);
         } else {
             echo "Fila con 'code' vacío ignorada\n";
             echo "Fila $rowNum - Código: \"$code\"\n";
