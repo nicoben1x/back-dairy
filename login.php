2086339f47123c177ed3b21a60c71df35f442e03
unknown <testprogramacion2023@outlook.com>
2023-09-19 10:33:04 -0300

    Inicio de sesion exitoso usando sesiones php

--- login.php
+++ login.php
@@ -3,12 +3,10 @@
 
 // Habilitar CORS (permitir solicitudes)
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST"); // Puedes ajustar los métodos HTTP permitidos según tu necesidad
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Credentials: true"); // Si deseas permitir el envío de cookies
-header("Content-Type: application/json"); // Establece el tipo de contenido de la respuesta
-
-// Resto del código PHP aquí
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
 
 // Importar la configuración de la base de datos desde database.php
 require 'database.php';
@@ -19,18 +17,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $jsonData = file_get_contents("php://input");
 
     // Decodifica los datos JSON en un objeto o un arreglo asociativo
-    $data = json_decode($jsonData, true); // El segundo parámetro true convierte en arreglo asociativo
+    $data = json_decode($jsonData, true);
 
     if ($data === null) {
         // Error al decodificar el JSON
-        echo json_encode(["message" => "Error al decodificar el JSON"]);
+        echo json_encode(["success" => false, "message" => "Error al decodificar el JSON"]);
     } else {
         // Verifica si los datos esperados están presentes en el arreglo
         if (isset($data['usernameOrEmail'], $data['password'])) {
             $usernameOrEmail = $data['usernameOrEmail'];
             $password = $data['password'];
 
-            // Consulta la contraseña cifrada y los datos del usuario almacenados en la base de datos
+            // Consulta los datos del usuario almacenados en la base de datos
             $sql = "SELECT id, nombre_completo, username, email, rol, contraseña FROM usuarios WHERE username = ? OR email = ?";
             try {
                 $stmt = $pdo->prepare($sql);
@@ -39,19 +37,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
                 if ($user && password_verify($password, $user['contraseña'])) {
                     // Las credenciales son correctas, el usuario ha iniciado sesión con éxito
+
+                    // Iniciar una sesión de PHP
+                    session_start();
+
+                    // Almacenar los datos del usuario en la sesión
+                    $_SESSION['user'] = $user;
+
+                    // Responder con un mensaje de inicio de sesión exitoso y los datos del usuario
                     echo json_encode(["success" => true, "message" => "Inicio de sesión exitoso", "user" => $user]);
                 } else {
                     // Las credenciales son incorrectas, el inicio de sesión ha fallado
                     echo json_encode(["success" => false, "message" => "Credenciales incorrectas"]);
                 }
             } catch (PDOException $e) {
-                echo json_encode(["message" => "Error al consultar la base de datos: " . $e->getMessage()]);
+                echo json_encode(["success" => false, "message" => "Error al consultar la base de datos: " . $e->getMessage()]);
             }
         } else {
-            echo json_encode(["message" => "No se recibieron todos los datos del formulario"]);
+            echo json_encode(["success" => false, "message" => "No se recibieron todos los datos del formulario"]);
         }
     }
 } else {
-    echo json_encode(["message" => "No se detectó una solicitud POST"]);
+    echo json_encode(["success" => false, "message" => "No se detectó una solicitud POST"]);
 }
 ?>
