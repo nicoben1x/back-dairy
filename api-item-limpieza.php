e12d19d751ba5dce93e9656039bb015c7a38f4a6
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-24 10:55:10 -0300

    Se hizo seccion de items de limpieza

---
+++ api-item-limpieza.php
@@ -0,0 +1,47 @@
+<?php
+// Habilita CORS para permitir solicitudes desde https://new.dairy.com.ar y otras
+// La IP es de computadora hogar.
+$allowedOrigins = [
+    "https://new.dairy.com.ar",
+    "http://192.168.100.40:3000",
+    "http://localhost:3000"
+];
+
+$origin = $_SERVER['HTTP_ORIGIN'];
+
+if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
+
+// Resto de tu código aquí....
+
+require 'database.php'; // Reemplaza 'database.php' con el nombre de tu archivo de configuración de base de datos
+
+function getItemsLimpieza() {
+    global $pdo; // $pdo debe ser la instancia de tu conexión PDO
+
+    try {
+        $query = "SELECT * FROM ItemLimpieza";
+        $statement = $pdo->prepare($query);
+        $statement->execute();
+        $itemsLimpieza = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        return $itemsLimpieza;
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(['message' => 'Something goes wrong (ItemLimpieza)']);
+        exit; // Termina la ejecución del script si hay un error
+    }
+}
+
+// Obtén los datos de ItemLimpieza
+$itemsLimpiezaData = getItemsLimpieza();
+
+// Convierte los datos en formato JSON y los envía como respuesta
+$response = [
+    'itemsLimpieza' => $itemsLimpiezaData
+];
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
