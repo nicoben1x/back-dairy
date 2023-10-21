39cf50548cb80ff557aaa53fcc04e6b73282b5a8
Nico Benavidez <testprogramacion2023@outlook.com>
2023-10-21 20:05:24 -0300

    Update excel funciona para quimicos y products pero por separado

--- update-excel-quimicos.php
+++ update-excel-quimicos.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php'; // Asegúrate de incluir la biblioteca para leer 
 require 'database.php'; // Incluye el archivo con la configuración de la conexión a la base de datos
 
 // Función para insertar o actualizar datos en la base de datos
-function insertOrUpdateDatos($productId, $code, $description, $presentation, $dealerPrice, $retailPrice, $pdo, $rowNum) {
+function insertOrUpdateDatos($quimicoId, $code, $description, $presentation, $dealerPrice, $retailPrice, $costoKilo, $pdo, $rowNum) {
     try {
         // Verifica y ajusta los valores
         if ($code === "#N/A" || $code === "#REF!" || $code === "#VALUE!") {
@@ -16,27 +16,32 @@ function insertOrUpdateDatos($productId, $code, $description, $presentation, $de
         // Verifica si retailPrice es numérico, de lo contrario, establece 0
         $retailPrice = is_numeric($retailPrice) ? number_format($retailPrice, 2, '.', '') : 0;
 
+         // Verifica si costoKilo es numérico, de lo contrario, establece 0
+         $costoKilo = is_numeric($costoKilo) ? number_format($costoKilo, 2, '.', '') : 0;
+
         // Consulta si 'code' ya existe en la base de datos
-        $stmt = $pdo->prepare('SELECT * FROM productNormal WHERE code = ?');
+        $stmt = $pdo->prepare('SELECT * FROM quimicoNormal WHERE code = ?');
         $stmt->execute([$code]);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($row) {
             // Si 'code' existe, actualiza la fila existente
-            $stmt = $pdo->prepare('UPDATE productNormal SET dealerPrice = ?, retailPrice = ?, description = ?, presentation = ? WHERE code = ?');
-            $stmt->execute([$dealerPrice, $retailPrice, $description, $presentation, $code]);
+            $stmt = $pdo->prepare('UPDATE quimicoNormal SET dealerPrice = ?, retailPrice = ?, description = ?, presentation = ?, costoKilo = ? WHERE code = ?');
+            $stmt->execute([$dealerPrice, $retailPrice, $description, $presentation, $costoKilo, $code]);
 
             echo "Registro actualizado para el código $code\n";
             echo "Dealer Price: " . number_format($dealerPrice, 2) . "\n";
             echo "Retail Price: " . number_format($retailPrice, 2) . "\n";
+            echo "Costo por Kilo: " . number_format($costoKilo, 2) . "\n";
         } else {
             // Si 'code' no existe, inserta una nueva fila
-            $stmt = $pdo->prepare('INSERT INTO productNormal (productId, code, description, presentation, dealerPrice, retailPrice) VALUES (?, ?, ?, ?, ?, ?)');
-            $stmt->execute([$productId, $code, $description, $presentation, $dealerPrice, $retailPrice]);
+            $stmt = $pdo->prepare('INSERT INTO quimicoNormal (quimicoId, code, description, presentation, dealerPrice, retailPrice, costoKilo) VALUES (?, ?, ?, ?, ?, ?, ?)');
+            $stmt->execute([$quimicoId, $code, $description, $presentation, $dealerPrice, $retailPrice, $costoKilo]);
 
             echo "Nuevo registro insertado para el código $code\n";
             echo "Dealer Price: " . number_format($dealerPrice, 2) . "\n";
             echo "Retail Price: " . number_format($retailPrice, 2) . "\n";
+            echo "Costo por Kilo: " . number_format($costoKilo, 2) . "\n";
         }
     } catch (PDOException $e) {
         // Registra el error y la información de la celda no procesada
@@ -46,6 +51,7 @@ function insertOrUpdateDatos($productId, $code, $description, $presentation, $de
         echo "Presentación: \"$presentation\"\n";
         echo "Dealer Price: \"$dealerPrice\"\n";
         echo "Retail Price: \"$retailPrice\"\n";
+        echo "Costo por Kilo: \"$costoKilo\"\n";
     }
 }
 
@@ -54,22 +60,23 @@ try {
     $inputFileName = './archivo.xlsm'; // Reemplaza con la ruta de tu archivo Excel
 
     $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
-    $worksheet = $spreadsheet->getSheetByName('Base');
+    $worksheet = $spreadsheet->getSheetByName('Quimicos');
 
-    for ($rowNum = 5; $rowNum <= 1311; $rowNum++) {
-        $productId = null;
+    for ($rowNum = 7; $rowNum <= 101; $rowNum++) {
+        $quimicoId = null; // Asegúrate de obtener el valor correcto para quimicoId
         $code = $worksheet->getCell('A' . $rowNum)->getValue();
         $description = $worksheet->getCell('B' . $rowNum)->getValue();
         $presentation = $worksheet->getCell('C' . $rowNum)->getValue();
         $dealerPrice = $worksheet->getCell('D' . $rowNum)->getCalculatedValue();
         $retailPrice = $worksheet->getCell('E' . $rowNum)->getCalculatedValue();
+        $costoKilo = $worksheet->getCell('F' . $rowNum)->getCalculatedValue();
 
         if (!is_string($code)) {
             $code = strval($code);
         }
 
         if (!empty($code)) {
-            insertOrUpdateDatos($productId, $code, $description, $presentation, $dealerPrice, $retailPrice, $pdo, $rowNum);
+            insertOrUpdateDatos($quimicoId, $code, $description, $presentation, $dealerPrice, $retailPrice, $costoKilo, $pdo, $rowNum);
         } else {
             echo "Fila con 'code' vacío ignorada\n";
             echo "Fila $rowNum - Código: \"$code\"\n";
