d7b77e618567464fb3eca61914cf56790602cd7b
Programador2023 <testprogramacion2023@outlook.com>
2023-11-25 21:46:27 -0300

    Se carga archivo pdf y clickeando sus codigos se buscan en el pdf

--- get-libro.php
+++ get-libro.php
@@ -14,7 +14,7 @@ if (in_array($origin, $allowedOrigins)) {
     header("Access-Control-Allow-Origin: $origin");
 }
 
-// Ruta al archivo PDF que deseas servirrr
+// Ruta al archivo PDF que deseas servir
 $pdfFilePath = './libro.pdf';
 
 // Asegúrate de establecer los encabezados MIME adecuados para el PDF
