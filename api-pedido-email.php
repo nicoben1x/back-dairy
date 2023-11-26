23e556437275cab97daa12cc6fd8867488fbd821
Programador2023 <testprogramacion2023@outlook.com>
2023-11-26 11:21:54 -0300

    Se envian los precios en los pedidos

--- api-pedido-email.php
+++ api-pedido-email.php
@@ -6,15 +6,11 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true"); // Si deseas permitir el envío de cookies
 header("Content-Type: application/json"); // Establece el tipo de contenido de la respuesta
 
-
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php'; // Asegúrate de que la ruta sea la correcta para incluir PHPMailer
-
-
 
 // Importar la configuración de la base de datos desde database.php
 require 'database.php';
@@ -31,93 +27,63 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Error al decodificar el JSON
         echo json_encode(["message" => "Error al decodificar el JSON"]);
     } else {
-        // Obtén el valor de "fecha" del JSON
+        // Obtén el valor de "selectedItems" y "usuariopedido" del JSON
         $selectedItems = $data['selectedItems'];
-
-        // Obtén el valor de "usuariostock" del JSON
         $usuariopedido = $data['usuariopedido'];
 
         // Conexión a la base de datos (utilizando la configuración de database.php)
         $conn = $pdo;
 
         if ($conn) {
-            
-
             // Si la operación fue exitosa, envía una respuesta de éxito
             $response = array('success' => true, 'message' => 'Cambios guardados exitosamente');
             echo json_encode($response);
-            
 
+            // Envía un correo electrónico después de actualizar la base de datos
+            $mail = new PHPMailer(true);
+            try {
+                $mail->isSMTP();
+                $mail->Host = 'mail.dairy.com.ar'; // Cambia esto a tu servidor SMTP
+                $mail->SMTPAuth = true;
+                $mail->Username = 'nico@dairy.com.ar'; // Cambia esto a tu dirección de correo
+                $mail->Password = 'tomatE77!'; // Cambia esto a tu contraseña de correo
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587; // Cambia esto al puerto SMTP adecuado
 
+                // Configurar la codificación de caracteres a UTF-8
+                $mail->CharSet = 'UTF-8';
 
+                $mail->setFrom('nico@dairy.com.ar', 'Nico Dairy');
+                $mail->addAddress('nicoben1x@gmail.com', 'Nicoben');
 
-                        // ... Tu código para actualizar la base de datos ...
+                $mail->Subject = 'Pedido Dairy Web';
+                $body = 'Se ha realizado el siguiente pedido por ' . $usuariopedido . ".\n\n";
 
-// Envía un correo electrónico después de actualizar la base de datos
+                foreach ($selectedItems as $item) {
+                    $body .= "Producto: " . $item['producto'] . ".\n";
+                    $body .= "Precio Unitario: $" . $item['precio'] . ".\n";
+                    $body .= "Cantidad: " . $item['cantidad'] . ".\n";
+                    $body .= "Precio Total: $" . $item['precioTotalProducto'] . ".\n\n";
+                }
 
-$mail = new PHPMailer(true);
-try {
-    
-    
-    $mail->isSMTP();
-    $mail->Host = 'mail.dairy.com.ar'; // Cambia esto a tu servidor SMTP
-    $mail->SMTPAuth = true;
-    $mail->Username = 'nico@dairy.com.ar'; // Cambia esto a tu dirección de correo
-    $mail->Password = 'tomatE77!'; // Cambia esto a tu contraseña de correo
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587; // Cambia esto al puerto SMTP adecuado
+                // Agregar el precio total final al correo
+                $body .= "Precio Total Final Estimado: $" . $data['precioTotalFinal'] . ".\n";
 
-       // Configurar laa codificación de caracteres a UTF-8
-       $mail->CharSet = 'UTF-8';
+                $mail->Body = $body;
 
-    $mail->setFrom('nico@dairy.com.ar', 'Nico Dairy');
-    $mail->addAddress('nuevonnncuenta@gmail.com', 'Nicoben');
+                $mail->SMTPOptions = [
+                    'ssl' => [
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true,
+                    ],
+                ];
 
-    
-    $mail->Subject = 'Pedido Dairy Web';
-$body = 'Se ha sido realizado el siguiente pedido. Realizado por ' . json_encode($usuariopedido) . ".\n\n";
-
-foreach ($selectedItems as $item) {
-    $product = $item['item'];
-    $quantity = $item['quantity'];
-    $body .= "Producto: " . $product['description'] . ".\n";
-    $body .= "Código: " . $product['code'] . ".\n";
-    $body .= "Presentación: " . $product['presentation'] . ".\n";
-    $body .= "Cantidad: " . $quantity . ".\n\n";
-}
-
-$mail->Body = $body;
-
-    $mail->SMTPOptions = [
-        'ssl' => [
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true,
-        ],
-    ];
-
-    $mail->send();
-
-
-
-        // Respuesta de éxito
-       
-    } catch (Exception $e) {
-        // Manejo de erroress
-       
-    }
-
-
-
-
-
-
-
-
-
-
-
-
+                $mail->send();
+            } catch (Exception $e) {
+                // Manejo de errores en el envío del correo
+                echo json_encode(["success" => false, "message" => "Error al enviar el correo electrónico: " . $mail->ErrorInfo]);
+            }
         } else {
             // Error de conexión a la base de datos
             $response = array('success' => false, 'message' => 'Error de conexión a la base de datos pedidos.');
