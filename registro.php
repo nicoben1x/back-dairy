00d3ddd0abd755cdad6f102a7258f8b63f503ee4
Programador2023 <testprogramacion2023@outlook.com>
2023-11-27 20:24:55 -0300

    Cuando se registra alguien nuevo me llega un email

--- registro.php
+++ registro.php
@@ -7,6 +7,11 @@ header("Access-Control-Allow-Credentials: true"); // Si deseas permitir el enví
 header("Content-Type: application/json"); // Establece el tipo de contenido de la respuesta
 
 // Resto del código PHP aquí
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; // Asegúrate de que la ruta sea la correcta para incluir PHPMailer
 
 // Importar la configuración de la base de datos desde database.php
 require 'database.php';
@@ -61,6 +66,46 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             $stmt_insert = $pdo->prepare($sql_insert);
                             $stmt_insert->execute([$nombre_completo, $username, $email, $nuevaContraseña, $rol]);
                             echo json_encode(["message" => "Registro exitoso"]);
+
+                            // Envía un correo electrónico después de actualizar la base de datos
+            $mail = new PHPMailer(true);
+            try {
+                $mail->isSMTP();
+                $mail->Host = 'mail.dairy.com.ar'; // Cambia esto a tu servidor SMTP
+                $mail->SMTPAuth = true;
+                $mail->Username = 'nico@dairy.com.ar'; // Cambia esto a tu dirección de correo
+                $mail->Password = 'tomatE77!'; // Cambia esto a tu contraseña de correo
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587; // Cambia esto al puerto SMTP adecuado
+
+                // Configurar la codificación de caracteres a UTF-8
+                $mail->CharSet = 'UTF-8';
+
+                $mail->setFrom('nico@dairy.com.ar', 'Nico Dairy');
+                $mail->addAddress('nicoben1x@gmail.com', 'Nicoben');
+
+                $mail->Subject = 'Registro de Usuario';
+                $body = 'Se ha registrado un nuevo usuario.';
+
+               
+                $mail->Body = $body;
+
+                $mail->SMTPOptions = [
+                    'ssl' => [
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true,
+                    ],
+                ];
+
+                $mail->send();
+            } catch (Exception $e) {
+                // Manejo de errores en el envío del correo
+                echo json_encode(["success" => false, "message" => "Error al enviar el correo electrónico: " . $mail->ErrorInfo]);
+            }
+
+
+
                         } catch (PDOException $e) {
                             echo json_encode(["message" => "Error al registrar: " . $e->getMessage()]);
                         }
