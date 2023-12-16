a374a6683af259b0985be4d31e9bbcfeacd956a0
nicoben1x <testprogramacion2023@outlook.com>
2023-12-16 15:17:33 -0300

    Otra forma de actualizar excel implementada

--- subir-excel.php
+++ subir-excel.php
@@ -5,44 +5,27 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
 
-function uploadAndReplaceFile($file) {
-    $targetDir = __DIR__ . "/"; // Directorio donde se ejecuta el script
-    $targetFileName = "archivo.xlsm"; // Nombre fijo del archivo
-    $targetFile = $targetDir . $targetFileName;
-    $uploadOk = 1;
-    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+$response = array(); // Array para almacenar la respuesta
 
-    // Permitir ciertos formatos de archivoo (en este caso, solo XLMS)
-    if ($fileType !== "xlsm") {
-        echo json_encode(array("message" => "Solo se permite el formato de archivo XLMS."));
-        $uploadOk = 0;
-    }
+if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
+    $nombre_archivo = 'archivo';
+    $carpeta_destino = 'excel/';
+    $ruta_archivo = $carpeta_destino . $nombre_archivo . '.xlsm';
 
-    // Si el archivo ya existe, intenta reemplazarlo
-    if (file_exists($targetFile)) {
-        if (!unlink($targetFile)) {
-            echo json_encode(array("message" => "Hubo un error al reemplazar el archivo existente."));
-            $uploadOk = 0;
-        }
-    }
-
-    // Verificar si $uploadOk está configurado en 0 por algún error
-    if ($uploadOk == 0) {
-        echo json_encode(array("message" => "El archivo no fue subido."));
+    // Mueve el archivo al directorio de destino
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $ruta_archivo)) {
+        $response['status'] = 'success';
+        $response['message'] = 'Archivo subido correctamente.';
     } else {
-        // Intentar subir el archivo
-        if (move_uploaded_file($file["tmp_name"], $targetFile)) {
-            echo json_encode(array("message" => "El archivo se ha subido y reemplazado con el nombre 'archivo.xlsm'."));
-        } else {
-            echo json_encode(array("message" => "Hubo un error al subir el archivo."));
-        }
+        $response['status'] = 'error';
+        $response['message'] = 'Error al mover el archivo.';
     }
+} else {
+    $response['status'] = 'error';
+    $response['message'] = 'Error al subir el archivo.';
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["file"])) {
-    uploadAndReplaceFile($_FILES["file"]);
-} else {
-    echo json_encode(array("message" => "No se recibió ningún archivo."));
-}
+// Devuelve la respuesta en formato JSON
+echo json_encode($response);
 ?>
 
